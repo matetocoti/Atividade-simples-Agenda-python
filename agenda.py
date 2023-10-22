f591da5a2e93c734857aef0030b331ddb9f71f61
Matheus Costa <matetogamesnew@gmail.com>
2023-10-22 19:51:09 -0300

    implementing remove contact functionality

--- agenda.py
+++ agenda.py
@@ -23,6 +23,18 @@ def agenda_de_telefone():
                     telefones = len(nome)
                     # Adicionando telefone ao contato
                     nome[f"Telefone{telefones}"] = input(f"\n\tDigite o telefone do contato:")
+                    break
+        # Excluindo contato
+        elif option == 3:
+            contato_index = -1
+            # Pegando o nome do contato|key
+            nome_busca = input(f"\n\tDigite o nome do contato:")
+            # Procurando contato na agenda
+            for nome in agenda:
+                contato_index += 1
+                if "nome" in nome and nome["nome"] == nome_busca:
+                    agenda.pop(contato_index)
+                    break
         # Buscando dados salvos do contato
         elif option == 4:
             # Pegando o nome do contato|key
@@ -31,9 +43,8 @@ def agenda_de_telefone():
             for nome in agenda:
                 if "nome" in nome and nome["nome"] == nome_busca:
                     print(f"\n\t{nome}")
+                    break
         # Encarrando programa
         elif option == 5:
             print(f"\n\tEncerrando...")
             break
-
-
